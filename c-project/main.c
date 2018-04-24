611f2bf2a0fd24f691d6dc1ca315f52f78c47bea
Leander Holm <leanderholm@gmail.com>
2018-04-24 11:53:33 +0200

    Added some pseudo code representing some of the conrol flow

--- c-project/main.c
+++ c-project/main.c
@@ -11,9 +11,56 @@
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+	// Setup
+	// Set port A to out
+	// Set port B to in 
+	// Set up sequence
+	// Set up current sequence length
+	
+    while (1)
     {
+		// call showSequenceState()
+		// call getPlayerInputState()
+		// call chechIfGameWon()
     }
+	
+	
+	
+	// showSequenceState()
+		// Blink the current sequence
+		
+		
+		
+	
+	// getPlayerInputState()
+		// Get button press
+		// Compare each pres to current sequence value
+			// If incorrect
+				// call showFailPattern()
+				// reset game
+				// return "wrong"
+		// call showCorrectPattern()
+		// return 0
+				
+	
+	
+	// chechIfGameWon()
+		// If full sequence has been shown
+			// call showWinPatternForever()
+	
+	
+	
+	// showFailPattern()
+		// resetGame()
+	
+	
+	
+	// showCorrectPattern()
+	
+	
+	
+	
+	// showWinPatternForever()
+	
 }
 
